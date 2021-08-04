3aace2def701749cb8925c3dd969b0f9f6ea046a
unknown <RockstarJay>
2021-08-04 14:12:53 +0530

    Added: welcome to the line comparison program

---
+++ src/com/jaybhagat/linecomparison/LineComparison.java
@@ -0,0 +1,8 @@
+package com.jaybhagat.linecomparison;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to The Line Comparison Computation Program");
+	}
+}
